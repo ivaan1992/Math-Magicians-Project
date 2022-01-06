c6d814e26d55986e93f84d0415bb06d9ef3bae5a
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-06 11:52:51 -0600

    Remove files

---
+++ src/components/Calculator.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+
+  render() {
+      return(
+          <div class="Calculator">
+             <h1>Hello world</h1>>
+          </div>
+      )
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+export default Calculator;;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './components'
+
 
 ReactDOM.render(
   <React.StrictMode>

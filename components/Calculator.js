3e8e2807a5d28f1d429d06ac1cc751eaaa573c7b
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-05 18:07:10 -0600

    Start creating the calculator

---
+++ components/Calculator.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+
+  render() {
+      return(
+          <div class="Calculator">
+              <input value='0' />
+              <button type='submit' className='calculatorBtn'>AC</button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+              <button type='submit' className='calculatorBtn'></button>
+          </div>
+      )
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+export default expression;
+

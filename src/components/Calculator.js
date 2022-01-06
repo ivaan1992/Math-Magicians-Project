8a0bf7ebc71ab59c9ff31fb714c61f171f3e1f41
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-06 17:05:22 -0600

    Fix calculator router

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 
 class Calculator extends Component {
   constructor(props) {
-      super(props);
-      this.state = {};
+    super(props);
+    this.state = {};
   }
 
   render() {
-      return (
-          <div className="Calculator">
-             <h1>Hello world</h1>>
-          </div>
-      );
+    return (
+      <div className="Calculator">
+        <h1>Hello world</h1>
+      </div>
+    );
   }
-} 
+}
 
 export default Calculator;
